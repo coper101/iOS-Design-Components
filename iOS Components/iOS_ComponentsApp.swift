ae59e921f4dcd1edc86ce5fb1fd0dc66d09e2b53
Daryl <darylgialolo@gmail.com>
2022-07-03 11:28:43 +0800

    Initial Commit

---
+++ iOS Components/iOS_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ComponentsApp.swift
+//  iOS Components
+//
+//  Created by Wind Versi on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
