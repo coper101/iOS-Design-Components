900e08924a8865360a843b67d92e4982fc5147ee
Daryl <darylgialolo@gmail.com>
2022-09-11 18:51:48 +0800

    Update: Font Weight

---
+++ iOS Components/Views/Text/FontWeightsView.swift
@@ -0,0 +1,20 @@
+//
+//  FontWeightsView.swift
+//  iOS Components
+//
+//  Created by Wind Versi on 3/7/22.
+//
+
+import SwiftUI
+
+struct FontWeightsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FontWeightsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FontWeightsView()
+    }
+}
